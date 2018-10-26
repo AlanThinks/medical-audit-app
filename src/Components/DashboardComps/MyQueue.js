14f0d1833dfcd92261d8805192ae3eeb7e27af19
AlanThinks <a@alanthinks.com>
2018-10-25 21:43:15 -0400

    MyQueue Progress

--- src/Components/DashboardComps/MyQueue.js
+++ src/Components/DashboardComps/MyQueue.js
@@ -5,6 +5,39 @@ export default class MyQueue extends Component {
     return (
       <div className="col my-queue">
         <h2>My Queue</h2>
+        <div className="btn-case row">
+          <div className="col-3">
+            <img
+              src="/media/specialties/respiratory.png"
+              alt="Respiratory Symbol"
+            />
+          </div>
+          <div className="col pl-3">
+            <h3>#90221</h3>
+          </div>
+        </div>
+        <div className="btn-case row">
+          <div className="col-3">
+            <img
+              src="/media/specialties/neurology.png"
+              alt="Respiratory Symbol"
+            />
+          </div>
+          <div className="col pl-3">
+            <h3>#777891</h3>
+          </div>
+        </div>
+        <div className="btn-case row">
+          <div className="col-3">
+            <img
+              src="/media/specialties/optometry.png"
+              alt="Respiratory Symbol"
+            />
+          </div>
+          <div className="col pl-3">
+            <h3>#50478</h3>
+          </div>
+        </div>
       </div>
     )
   }
