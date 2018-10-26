b48229a20dcd613f804c74f3e99df7846e38fb8a
AlanThinks <a@alanthinks.com>
2018-10-26 06:42:43 -0400

    MyQueue Initial Done; btn-case styles

--- src/Components/DashboardComps/MyQueue.js
+++ src/Components/DashboardComps/MyQueue.js
@@ -14,28 +14,54 @@ export default class MyQueue extends Component {
           </div>
           <div className="col pl-3">
             <h3>#90221</h3>
+            <i class="fas fa-check" />
+            <div class="case-description">
+              <ul class="tags">
+                <li>Princeton Hospital</li>
+                <li>Memorial West</li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="btn-case row">
           <div className="col-3">
             <img
-              src="/media/specialties/neurology.png"
+              src="/media/specialties/dentistry.png"
               alt="Respiratory Symbol"
             />
           </div>
           <div className="col pl-3">
-            <h3>#777891</h3>
+            <h3>#73221</h3>
+            <i class="fas fa-check" />
+
+            <div class="case-description">
+              <ul class="tags">
+                <li>Princeton Hospital</li>
+                <li>Memorial West</li>
+                <li>Radiology</li>
+                <li>Emergency</li>
+              </ul>
+              <div className="balance">
+                Balance: <span>$12,021.97</span>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="btn-case row">
+        <div className="row btn-case complete ">
           <div className="col-3">
-            <img
-              src="/media/specialties/optometry.png"
-              alt="Respiratory Symbol"
-            />
+            <img src="/media/specialties/bones.png" alt="Respiratory Symbol" />
           </div>
           <div className="col pl-3">
-            <h3>#50478</h3>
+            <h3>#10034</h3>
+            <i class="fas fa-check" />
+            <div class="case-description">
+              <ul class="tags">
+                <li>Princeton Hospital</li>
+                <li>Memorial West</li>
+                <li>Radiology</li>
+                <li>Emergency</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
